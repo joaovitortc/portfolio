a9e42b7ec53d6f29b7c74a304f65b00458d23508
Joao Vitor <jvtopanotti-da-cunha@myseneca.ca>
2024-05-07 12:10:11 -0400

    Fixed: Navbar on mobile screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,16 +3,16 @@ import React from "react";
 const NavBar: React.FC = () => {
   return (
     <nav className='navbar-top'>
-    <div id='navbar' className="w-full flex flex-wrap items-center justify-between mx-auto p-4 font-medium md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+    <div id='navbar' className="w-full flex flex-wrap items-center justify-between mx-auto p-2 font-small md:space-x-8 rtl:space-x-reverse border-0">
         <div className="text-white">
             Joao Cunha Portfolio
         </div>
-      <div className="hidden w-full md:block md:w-auto">
-        <ul id='navbar' className="flex flex-col p-4 md:p-0 mt-4 bg-transparent border border-gray-100 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+      <div className="w-full block w-auto">
+        <ul className="flex flex-row p-0 mt-4 space-x-2 sm:space-x-8 rtl:space-x-reverse mt-0 border-0">
           <li>
             <a
               href="#about"
-              className="block py-2 px-3 text-white md:bg-transparent md:p-0 dark:text-white md:p-0 dark:text-white md:dark:hover:text-blue-500 md:dark:hover:bg-transparent"
+              className="block py-2 px-3 text-white rounded p-0 dark:text-white dark:hover:bg-gray-700 sm:px-1"
               aria-current="page"
             >
               About
@@ -21,7 +21,7 @@ const NavBar: React.FC = () => {
           <li>
             <a
               href="#skills"
-              className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white md:dark:hover:bg-transparent"
+              className="block py-2 px-3 rounded border-0 p-0 dark:text-white dark:hover:bg-gray-700 sm:px-1"
             >
               Skills
             </a>
@@ -29,7 +29,7 @@ const NavBar: React.FC = () => {
           <li>
             <a
               href="#projects"
-              className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              className="block py-2 px-3  rounded border-0 p-0 dark:text-white dark:hover:bg-gray-700 sm:px-1"
             >
               Projects
             </a>
